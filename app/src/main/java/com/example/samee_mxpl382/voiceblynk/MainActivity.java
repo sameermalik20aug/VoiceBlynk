3276e1a3dfa0ecd75cddeba3cacc92702208f6e5
shubhamgulati11 <shubhamgulati11may@gmail.com>
2018-11-02 23:10:42 +0530

    Added Custom Tabs

--- app/src/main/java/com/example/samee_mxpl382/voiceblynk/MainActivity.java
+++ app/src/main/java/com/example/samee_mxpl382/voiceblynk/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.samee_mxpl382.voiceblynk;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.speech.RecognizerIntent;
 import android.support.annotation.Nullable;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +13,8 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -50,7 +55,21 @@ public class MainActivity extends AppCompatActivity {
             case 10:
                 if (resultCode == RESULT_OK && data != null) {
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    textView.setText(result.get(0));
+                    String q=result.get(0);
+                    textView.setText(q);
+                    try {
+                        String search=URLEncoder.encode(q,"UTF-8");
+                        String url="https://www.google.co.in/search?q="+search+"&rlz=1C1CHBF_enIN821IN821&oq="+search+"&aqs=chrome..69i57j0l5.1618j0j7&sourceid=chrome&ie=UTF-8";
+                        CustomTabsIntent.Builder builder= new CustomTabsIntent.Builder();
+                        builder.setToolbarColor(ContextCompat.getColor(this, R.color.colorAccent));
+                        CustomTabsIntent customTabsIntent = builder.build();
+                        customTabsIntent.launchUrl(this, Uri.parse(url));
+
+
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+
                 }
                 break;
         }
